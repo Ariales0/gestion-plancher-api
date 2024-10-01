58ef8298ed9f8fb6eac8d1474dde5758bc29e076
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2024-10-01 11:57:06 -0400

    Documentation
    Configuration de la doc swagger sécurisé
    Ajout d'un fichier rest pour tester les requêtes
    Modification de package json pour run l'application avec un npm start

--- server.js
+++ server.js
@@ -19,17 +19,23 @@ require("dotenv").config(); // Importation du module dotenv
 const express = require("express"); // Importation du module express
 const sequelize = require("./config/db"); // Importation de la connexion à la base de données
 const userRoutes = require("./routes/userRoutes"); // Importation des routes utilisateur
+const swaggerJSDoc = require("swagger-jsdoc"); // Importation du module swagger-jsdoc
+const swaggerUi = require("swagger-ui-express"); // Importation du module swagger-ui-express
 
 // <----------- Section configuration ------------->
 const port = process.env.PORT; // Configuration du port
+const environment = process.env.NODE_ENV; // Configuration de l'environnement
 const app = express(); // Création de l'application express
 app.use(express.json()); // Utilisation du middleware pour comprendre les requêtes en JSON
 
 // <-----------Section base de données------------->
 // === Tester la connexion à la base de données ===
-sequelize.authenticate().then(() => {
+sequelize
+  .authenticate()
+  .then(() => {
     console.log("Connexion à la base de données réussie !");
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.error("Impossible de se connecter à la base de données:", error);
   });
 
@@ -38,6 +44,35 @@ sequelize.authenticate().then(() => {
 app.listen(port, () => {
   console.log(`L'API est en ligne !`);
 });
+
+// <----------- Section swagger ------------->
+// === Configuration de la documentation swagger ===
+const swaggerOptions = {
+  definition: { 
+    openapi: "3.0.0",
+    info: {
+      title: "API Gestion Plancher",
+      version: "1.0.0",
+      description: "API pour la gestion des planchers",
+      contact: {
+        name: "Melyos",
+        email: "melyos@shultersys.org",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`, 
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], 
+};
+
+// === Afficher la documentation swagger en développement ===
+if (environment === "development") {
+  const swaggerDocs = swaggerJSDoc(swaggerOptions);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+}
 
 // <----------- Section routes ------------->
 // === Route de bienvenue ===
